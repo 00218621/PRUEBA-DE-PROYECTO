00af6bcc075e486ee3634f6bca2df865d6c45e2e
00218621 <00218621@uca.edu.sv>
2022-06-12 16:13:50 -0600

    proyecto

---
+++ Proyecto/Proyecto/frmInicioSesion.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class frmInicioSesion : Form
+    {
+        public frmInicioSesion()
+        {
+            InitializeComponent();
+        }
+    }
+}
